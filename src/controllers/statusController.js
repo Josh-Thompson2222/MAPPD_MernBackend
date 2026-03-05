69ce34d67606ec701df464391d8b21989a78a77f
Josh Thompson <G00422756@atu.ie>
2026-03-05 11:41:50 +0000

    Added new folders

---
+++ src/controllers/statusController.js
@@ -0,0 +1,9 @@
+// src/controllers/statusController.js
+exports.getStatus = (req, res) => {
+  res.json({
+    status: "Online",
+    message: "AWS Backend is reachable!",
+    owner: "Josh Thompson",
+    timestamp: new Date()
+  });
+};
